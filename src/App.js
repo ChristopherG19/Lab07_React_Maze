0e787152bdd8b2d43eda8b44167400674f635572
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-04-20 22:27:49 -0600

    Add files via upload
    Primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Maze from "./Components/Maze"
+const App = () => {
+
+    return (
+        <div>
+            <h1 id ="Titulo">Amazeing</h1>
+            <Maze/>
+        </div>
+    )
+}
+
+export default App
