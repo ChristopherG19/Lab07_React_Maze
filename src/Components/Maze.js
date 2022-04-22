a5dfa8775b4f8fd683ba72c1507129bd13977a21
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-04-21 21:48:13 -0600

    Actualización

--- src/Components/Maze.js
+++ src/Components/Maze.js
@@ -1,30 +1,23 @@
-import {useState} from 'react'
-import {useEffect} from 'react'
 
-const Maze = () => {
-    
-    const MazeList = ({ Maze }) => <div>{Maze.map(row => <p>{String(row).concat(" ").replaceAll("", " ").split(",")}</p>)}</div>
-    
-    const [Maze, setMaze] = useState([])
+const PrintMaze = ({laberinto, width, height}) => {
 
-    useEffect(() => {
-
-        const urlOrigen = 'https://maze.juanelcaballo.club/?type=json&w=%&h=#'
-        const NewWeight = urlOrigen.replace('%',4)
-        const urlFinal = NewWeight.replace('#',4)
-        
-        fetch(urlFinal)
-            .then((response) => response.json())
-            .then((responseInJSON) =>{
-                setMaze(responseInJSON)
-            })
-    }, [])
+    console.log("Laberinto recibido: ",laberinto)
 
     return (
-        <div id="Maze">
-            <MazeList Maze = {Maze}/>
+        <div className = 'tablero' id = 'grid' >
+            {
+                laberinto.map( linea =>
+                    linea.map( element => {
+                        if (element === "-" || element === "|" || element === "+") {
+                          return <h1>{element}</h1>
+                        }
+                        return <p>{element}</p>
+                    })
+                )
+            }
+    
         </div>
     )
 }
 
-export default Maze
+export default PrintMaze
