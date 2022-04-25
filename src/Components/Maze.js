d7d9d224fe9c8a03653e31a4f6434371f73db805
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-04-24 22:05:47 -0600

    Actualización Laberinto

--- src/Components/Maze.js
+++ src/Components/Maze.js
@@ -1,21 +1,31 @@
-
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react'
+import React from "react"
 const PrintMaze = ({laberinto, width, height}) => {
 
-    console.log("Laberinto recibido: ",laberinto)
+    const style = css`
+    color: #fff;
+    background: black;
+    display: grid;
+    text-align: center;
+    grid-template-columns: repeat(${height}, 40px);
+    grid-template-rows: repeat(${width}, 20px);
+    `
 
     return (
-        <div className = 'tablero' id = 'grid' >
+        <div id = 'gridMaze' css={style}>
             {
-                laberinto.map( linea =>
-                    linea.map( element => {
-                        if (element === "-" || element === "|" || element === "+") {
-                          return <h1>{element}</h1>
+                laberinto.map( row =>
+                    row.map( part => {
+                        if (part == "-" || part == "|" || part == "+") {
+                            return <div>{part}</div>
                         }
-                        return <p>{element}</p>
+                        else {
+                            return <div>{part}</div>
+                        }
                     })
                 )
             }
-    
         </div>
     )
 }
